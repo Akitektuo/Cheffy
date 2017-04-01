e96613e6013c67ace8428d3f5b0b5535a64880dd
ioansiran <ioansiran17@gmail.com>
2017-04-01 21:08:40 +0300

    Updated Backend

--- backend/data.js
+++ backend/data.js
@@ -1,73 +1,80 @@
-
 exports.recipes = [
     {
       id:0,
-      name:"Eminescu",
+      name:"Dummy",
       content:"Pastele făinoase, sunt un produs alimentar obținut prin uscarea aluatului nedospit de făină cu apă la care se poate adăuga de asemenea și sare sau ouă.",
       ingredients:[
         "Fler", "piper", "cianura", "cocaina", "stele cazatoare"
       ],
+      weights:[
+        "100g", "200 l", "1 lingurita", "1 praf", "1 kg"
+      ],
       duration:3807664,
-      picture:"dickpic.jpg"
+      picture:"dummy.jpg"
     },
     {
       id:1,
-      name:"Pasta",
+      name:"Clatite",
       content:"Pastele făinoase, sunt un produs alimentar obținut prin uscarea aluatului nedospit de făină cu apă la care se poate adăuga de asemenea și sare sau ouă.",
       ingredients:[
         "zahar", "piper", "cianura", "cocaina", "stele cazatoare"
       ],
+      weights:[
+        "100g", "200 l", "1 lingurita", "1 praf", "1 kg"
+      ],
       duration:3472985,
-      picture:"dickpic.jpg"
+      picture:"clatite.jpg"
     },
     {
       id:2,
-      name:"Macaronni",
+      name:"Desert",
       content:"Senzația de euforie se manifesta în primele secunde de la consum, iar în cazul în care drogul este fumat, efectul persista pana la 24 de ore. Fiind un puternic stimulent al SNC, chiar și în doze mici, poate spori concentrarea și energia fizica, dar scade apetitul și în timp provoacă leziuni cerebrale.",
       ingredients:[
         "meth", "puss ", "cianura", "cocaina", "stele cazatoare"
       ],
+      weights:[
+        "100g", "200 l", "1 lingurita", "1 praf", "1 kg"
+      ],
       duration:2696944,
-      picture:"dickpic.jpg"
+      picture:"desert.jpg"
     },
     {
       id:3,
-      name:"Pasta",
+      name:"Pizza",
       content:"Cocaina este un alcaloid tropanic cristalin, care se obține din frunzele arbustului de coca.",
       ingredients:[
         "zahar", "piper", "cianura", "cocaina", "stele cazatoare"
       ],
+      weights:[
+        "100g", "200 l", "1 lingurita", "1 praf", "1 kg"
+      ],
       duration:2806947,
-      picture:"dickpic.jpg"
+      picture:"pizza.jpg"
     },
     {
       id:4,
-      name:"Pasta",
+      name:"Alta Pizza",
       content:"Pula este un oraș în cantonul Istria, Croația, având o populație de 57.460 de locuitori. ",
       ingredients:[
         "zahar", "piper", "cianura", "cocaina", "stele cazatoare"
       ],
+      weights:[
+        "100g", "200 l", "1 lingurita", "1 praf", "1 kg"
+      ],
       duration:4532383,
-      picture:"dickpic.jpg"
+      picture:"alta-pizza.jpg"
     },
     {
       id:5,
-      name:"Pasta",
+      name:"Prajitura",
       content:"Pastele făinoase, sunt un produs alimentar obținut prin uscarea aluatului nedospit de făină cu apă la care se poate adăuga de asemenea și sare sau ouă.",
       ingredients:[
         "zahar", "piper", "cianura", "cocaina", "stele cazatoare"
+      ],
+      weights:[
+        "100g", "200 l", "1 lingurita", "1 praf", "1 kg"
       ],
       duration:2455552,
-      picture:"dickpic.jpg"
-    },
-    {
-      id:6,
-      name:"Pasta",
-      content:"Pastele făinoase, sunt un produs alimentar obținut prin uscarea aluatului nedospit de făină cu apă la care se poate adăuga de asemenea și sare sau ouă.",
-      ingredients:[
-        "zahar", "piper", "cianura", "cocaina", "stele cazatoare"
-      ],
-      duration:2675880,
-      picture:"dickpic.jpg"
+      picture:"prajitura.jpg"
     }
   ];
