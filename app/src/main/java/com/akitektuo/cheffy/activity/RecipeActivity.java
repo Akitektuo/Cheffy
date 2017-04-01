9c9c7fb88cc6e008a8ce396017b2be1702962e5e
Akitektuo <alexcopindean@yahoo.com>
2017-04-02 01:37:50 +0300

    Tests of getting data from database complete

--- app/src/main/java/com/akitektuo/cheffy/activity/RecipeActivity.java
+++ app/src/main/java/com/akitektuo/cheffy/activity/RecipeActivity.java
@@ -20,8 +20,15 @@ import com.akitektuo.cheffy.database.DatabaseHelper;
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
+import static com.akitektuo.cheffy.util.Constant.CURSOR_DESCRIPTION;
+import static com.akitektuo.cheffy.util.Constant.CURSOR_DURATION;
+import static com.akitektuo.cheffy.util.Constant.CURSOR_INGREDIENT;
+import static com.akitektuo.cheffy.util.Constant.CURSOR_PICTURE;
+import static com.akitektuo.cheffy.util.Constant.CURSOR_QUANTITY;
 import static com.akitektuo.cheffy.util.Constant.CURSOR_RECIPE;
 import static com.akitektuo.cheffy.util.Constant.KEY_NAME;
+import static com.akitektuo.cheffy.util.Tool.convertStringToList;
+import static com.akitektuo.cheffy.util.Tool.getBitmapForName;
 import static com.akitektuo.cheffy.util.Tool.setListViewHeightBasedOnItems;
 
 public class RecipeActivity extends Activity {
@@ -47,19 +54,58 @@ public class RecipeActivity extends Activity {
         TextView textTitle = (TextView) findViewById(R.id.text_title);
         imageFood = (ImageView) findViewById(R.id.image_food);
         ListView listIngredients = (ListView) findViewById(R.id.list_ingredients);
+        TextView textDescription = (TextView) findViewById(R.id.text_recipe);
+        TextView textDuration = (TextView) findViewById(R.id.text_time);
         database = new DatabaseHelper(this);
         Cursor cursor = database.getRecipeForName(getIntent().getStringExtra(KEY_NAME));
         if (cursor.moveToFirst()) {
             textTitle.setText(cursor.getString(CURSOR_RECIPE));
-
-            ArrayList<IngredientItem> ingredients = new ArrayList<>();
-            for (int i = 0; i < 10; i++) {
-                ingredients.add(new IngredientItem("ingredient_" + i, ((i + 1) * 50) + " g"));
+            textDescription.setText(cursor.getString(CURSOR_DESCRIPTION));
+            ArrayList<String> ingredients = convertStringToList(cursor.getString(CURSOR_INGREDIENT));
+            ArrayList<String> quantities = convertStringToList(cursor.getString(CURSOR_QUANTITY));
+            ArrayList<IngredientItem> ingredientItems = new ArrayList<>();
+            for (int i = 0; i < ingredients.size(); i++) {
+                ingredientItems.add(new IngredientItem(ingredients.get(i), quantities.get(i)));
+                System.out.println(ingredientItems.get(i).getIngredient() + " " + ingredientItems.get(i).getQuantity());
             }
-            listIngredients.setAdapter(new IngredientAdapter(this, ingredients));
+            listIngredients.setAdapter(new IngredientAdapter(this, ingredientItems));
             if (!setListViewHeightBasedOnItems(listIngredients)) {
                 finish();
             }
+            textDuration.setText(convertMillisToTime(cursor.getLong(CURSOR_DURATION)));
+            imageFood.setImageBitmap(getBitmapForName(this, cursor.getString(CURSOR_PICTURE)));
         }
+    }
+
+    private String convertMillisToTime(long time) {
+        int seconds = (int) time / 1000, minutes = 0, hours = 0;
+        while (seconds > 59) {
+            minutes++;
+            seconds -= 60;
+        }
+        while (minutes > 59) {
+            hours++;
+            minutes -= 60;
+        }
+        String sec = "00", min = "00", h = "00";
+        if (seconds != 0) {
+            sec = String.valueOf(seconds);
+            if (sec.length() == 1) {
+                sec = "0" + sec;
+            }
+        }
+        if (minutes != 0) {
+            min = String.valueOf(minutes);
+            if (min.length() == 1) {
+                min = "0" + min;
+            }
+        }
+        if (hours != 0) {
+            h = String.valueOf(hours);
+            if (h.length() == 1) {
+                h = "0" + h;
+            }
+        }
+        return h + ":" + min + ":" + sec;
     }
 }
