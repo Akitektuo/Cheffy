31b38c8b1423737ee564fa4f52c42ae8f4cc5de5
Akitektuo <alexcopindean@yahoo.com>
2017-04-05 20:31:46 +0300

    Added saving and loading Bitmap methods

--- app/src/main/java/com/akitektuo/cheffy/util/Tool.java
+++ app/src/main/java/com/akitektuo/cheffy/util/Tool.java
@@ -1,6 +1,7 @@
 package com.akitektuo.cheffy.util;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.View;
@@ -9,6 +10,11 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -57,5 +63,38 @@ public class Tool {
         Bitmap resizedBitmap = BitmapFactory.decodeResource(context.getResources(), context.getResources().getIdentifier(image, "drawable", context.getPackageName()));
         resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, new ByteArrayOutputStream());
         return resizedBitmap;
+    }
+
+    private void saveToInternalStorage(Context context, Bitmap image, String name) {
+        ContextWrapper cw = new ContextWrapper(context);
+        File directory = cw.getDir("image_food", Context.MODE_PRIVATE);
+        File myPath = new File(directory, name + ".jpg");
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(myPath);
+            image.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                assert fos != null;
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Bitmap loadImageFromStorage(Context context, String name) {
+        Bitmap bitmap = null;
+        ContextWrapper cw = new ContextWrapper(context);
+        File directory = cw.getDir("image_food", Context.MODE_PRIVATE);
+        try {
+            File f = new File(directory, name + ".jpg");
+            bitmap = BitmapFactory.decodeStream(new FileInputStream(f));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 }
